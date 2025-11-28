c1602773dd74b728a08bfe4473984f4854617581
Trupal-Gondaliya <trupalgondaliya@gmail.com>
2025-11-28 12:50:42 +0530

    implement react router dom for fetch selected product details

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
 
 function App() {
 
   return (
     <>
-      <h1 className="text-amber-400">App Component</h1>
+      <Header />
+      <Outlet />
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,39 @@
-import { StrictMode } from 'react'
+import { StrictMode , lazy, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
+const ProductList = lazy(() => import("./components/ProductList.jsx"));
+const ProductDetail = lazy(() => import("./components/ProductDetail.jsx"));
+
+const appRouter = createBrowserRouter([
+  {
+    path : "/",
+    element : <App />,
+    children : [
+      {
+        path : "/",
+        element : (
+          <Suspense fallback={<div>Loading...</div>}>
+            <ProductList />
+          </Suspense>
+        )
+      },
+      {
+        path : "/product/:id",
+        element : (
+          <Suspense fallback={<div>Loading....</div>}>
+            <ProductDetail />
+          </Suspense>
+        )
+      }
+    ],
+  }
+])
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={appRouter} />
   </StrictMode>,
 )
