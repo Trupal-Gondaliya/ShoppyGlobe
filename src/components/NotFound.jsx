a85b5ab096e76430a6d795a75ee55f640d6e7f9a
Trupal-Gondaliya <trupalgondaliya@gmail.com>
2025-12-03 13:02:43 +0530

    add error details using useRouteError

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,7 +1,8 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useRouteError } from "react-router-dom";
 
 function NotFound() {
   const location = useLocation(); 
+  const error = useRouteError();
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 p-6">
@@ -10,6 +11,14 @@ function NotFound() {
       <h2 className="text-3xl font-semibold text-gray-800 mb-2">
         Page Not Found
       </h2>
+
+      <div className="text-red-500 italic mb-4">
+        {error && (
+            <p>
+              Error Details: <span>{error.statusText || error.message}</span>
+            </p>
+        )}
+      </div>
 
       <p className="text-lg text-gray-600 mb-6">
         The URL <span className="text-red-700 font-bold">{location.pathname}</span> does not exist.
