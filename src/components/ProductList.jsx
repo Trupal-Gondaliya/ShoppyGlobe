7f7334b821701abec70a73ee79e1d6092aa24761
Trupal-Gondaliya <trupalgondaliya@gmail.com>
2025-12-03 13:01:03 +0530

    update product search to filter by tags and title

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -4,7 +4,7 @@ import { useSelector } from "react-redux";
 
 function ProductList() {
     const { products, loading, error } = useFetchProducts();
-    const searchTerm = useSelector((state) => state.cart.searchItem); 
+    const searchTerm = useSelector((state) => state.cart.searchItem);
 
     if (loading) {
         return <div className="text-center mt-10 text-xl">Loading products...</div>;
@@ -13,18 +13,35 @@ function ProductList() {
         return <div className="text-center mt-10 text-red-500">Error: {error}</div>;
     }
 
-    const filteredProducts = products.filter(product => 
-        product.title.toLowerCase().includes(searchTerm.toLowerCase())
+    // Filterd logic
+    const filteredProducts = products.filter(product => {
+        const lowerChar = searchTerm.toLowerCase();
+        const matchesTitle = product.title.toLowerCase().includes(lowerChar);
+        const matchesTags = product.tags?.some((tag) =>
+            tag.toLowerCase().includes(lowerChar));
+        return matchesTitle || matchesTags;
+    }
     );
 
     return (
         <div className="container mx-auto p-4">
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-                {filteredProducts.map((product) => (
-                    // Passing data via props and using a unique key 
-                    <ProductItem key={product.id} product={product} />
-                ))}
-            </div>
+            {filteredProducts.length === 0 ? (
+                <div className="text-center mt-20">
+                    <h2 className="text-2xl font-bold text-gray-600">No products found</h2>
+                    <p className="text-red-400 mt-2">
+                        We couldn't find any matches for "<span className="font-semibold text-black">{searchTerm}</span>".
+                    </p>
+                    <p className="text-gray-400">Try searching for generic terms like "Beauty", "Perfumes", or "Food".</p>
+                </div>
+            ) : (
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+                    {filteredProducts.map((product) => (
+                        // Passing data via props and using a unique key 
+                        <ProductItem key={product.id} product={product} />
+                    ))}
+                </div>
+            )
+            }
         </div>
     );
 };
